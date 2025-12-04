427be24fd04c7347a95907b2608bf047fb0a253f
quanlykhachsan <nguoikhongten02022005@gmail.com>
2025-12-04 19:32:00 +0700

    NganHangDbContext

---
+++ DAL/NganHangDbContext.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class NganHangDbContext : DbContext
+    {
+        public NganHangDbContext(DbContextOptions<NganHangDbContext> tuyChon) : base(tuyChon) { }
+
+        public DbSet<KhachHang> KhachHangs { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<KhachHang>()
+                .HasIndex(kh => kh.Email)
+                .IsUnique();
+        }
+    }
+    public class KhachHang
+    {
+        public int Id { get; set; }
+        public string HoTen { get; set; } = "";
+        public string Email { get; set; } = "";
+        public string SoDienThoai { get; set; } = "";
+        public string MatKhauHash { get; set; } = "";
+    }
+
+}
