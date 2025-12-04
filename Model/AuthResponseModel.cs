68b9b1f6b9fd6099c57149c035f07bfb08fa669c
quanlykhachsan <nguoikhongten02022005@gmail.com>
2025-12-04 17:10:16 +0700

    update

---
+++ Model/AuthResponseModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    internal class AuthResponseModel
+    {
+        public string Token { get; set; } = "";
+        public CustomerModel Customer { get; set; } = new();
+    }
+}
