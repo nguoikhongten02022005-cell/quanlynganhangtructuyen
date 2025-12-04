81e07713a10bec1db19141b32ea8a349b10f4ced
quanlykhachsan <nguoikhongten02022005@gmail.com>
2025-12-04 19:44:29 +0700

    KhachHangDAL

---
+++ DAL/KhachHangDAL.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class KhachHangDAL
+    {
+        private readonly NganHangDbContext _db;
+        public KhachHangDAL(NganHangDbContext db) => _db = db;
+
+        public Task<KhachHang?> LayTheoIdAsync(int id)
+            => _db.KhachHangs.FirstOrDefaultAsync(kh => kh.Id == id);
+
+        public Task<KhachHang?> LayTheoEmailAsync(string email)
+            => _db.KhachHangs.FirstOrDefaultAsync(kh => kh.Email == email);
+
+        public async Task ThemAsync(KhachHang kh)
+        {
+            _db.KhachHangs.Add(kh);
+            await _db.SaveChangesAsync();
+        }
+
+        public async Task CapNhatAsync(KhachHang kh)
+        {
+            _db.KhachHangs.Update(kh);
+            await _db.SaveChangesAsync();
+        }
+    }
+}
