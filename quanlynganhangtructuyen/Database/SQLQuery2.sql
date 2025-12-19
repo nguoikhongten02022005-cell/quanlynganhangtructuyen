12d389a63f32e01cbcba9e8cb93e3eeb008b60e8
DuyNam <duynamnguyen0995@gmail.com>
2025-12-19 15:49:12 +0700

    sql

---
+++ quanlynganhangtructuyen/Database/SQLQuery2.sql
@@ -0,0 +1,10 @@
+﻿USE QuanLyNganHangTrucTuyen;
+GO
+
+-- Bổ sung thông tin KYC cho KhachHang
+ALTER TABLE KhachHang
+ADD 
+    AnhCCCDTruoc VARCHAR(255),
+    AnhCCCDSau VARCHAR(255),
+    ThoiGianGuiKYC DATETIME;
+GO
