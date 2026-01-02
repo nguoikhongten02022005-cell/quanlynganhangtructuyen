f5382ec101cf63a1cbad3fb8e45ec4d0ed53a90a
DuyNam2005 <duynamnguyen0995@gmail.com>
2026-01-02 13:34:58 +0700

    Thêm API GET /api/account/my-account để lấy thông tin tài khoản và số dư

---
+++ quanlynganhangtructuyen/Controllers/AccountController.cs
@@ -0,0 +1,38 @@
+﻿using BLL.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace quanlynganhangtructuyen.Controllers
+{
+    [ApiController]
+    [Route("api/account")]
+    [Authorize(Roles = "CUSTOMER")]
+    public class AccountController : ControllerBase
+    {
+        private readonly IKhachHangService _dichVuKhachHang;
+
+        public AccountController(IKhachHangService dichVuKhachHang)
+        {
+            _dichVuKhachHang = dichVuKhachHang;
+        }
+
+        [HttpGet("my-account")]
+        public async Task<IActionResult> LayThongTinTaiKhoan()
+        {
+            var maNguoiDungStr = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            if (!int.TryParse(maNguoiDungStr, out var maNguoiDung))
+                return Unauthorized(new { thongBao = "Token không hợp lệ." });
+
+            try
+            {
+                var thongTinTaiKhoan = await _dichVuKhachHang.LayThongTinTaiKhoanAsync(maNguoiDung);
+                return Ok(thongTinTaiKhoan);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { thongBao = ex.Message });
+            }
+        }
+    }
+}
