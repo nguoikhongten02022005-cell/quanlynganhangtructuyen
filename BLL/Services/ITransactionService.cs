35035cc2af396cea105d74f8923a07a2682b0124
HoangViet <viet17510@gmail.com>
2026-01-04 15:38:20 +0700

    Implement LayLichSuGiaoDichAsync trong TransactionService

--- BLL/Services/ITransactionService.cs
+++ BLL/Services/ITransactionService.cs
@@ -7,5 +7,6 @@ namespace BLL.Services
         Task<object> TraCuuTaiKhoanNhanAsync(string soTaiKhoan);
         Task<object> TaoGiaoDichVoiOTPAsync(int maNguoiDung, string soTaiKhoanNhan, decimal soTien, string noiDung);
         Task<object> XacNhanOTPVaChuyenTienAsync(int maGiaoDich, string maOTP);
+        Task<object> LayLichSuGiaoDichAsync(int maNguoiDung, int pageSize = 20, int pageNumber = 1);
     }
 }
